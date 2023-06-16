119182742bb08c75e07c344263b81dcff743b8a2
Jarir Ahmed Deedat <129315010+jarircse16@users.noreply.github.com>
2023-06-16 20:42:36 +0600

    Java Practice
    Java Practice

---
+++ IdeaProjects/Pactice/src/Main.java
@@ -0,0 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        System.out.println(5+3);
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+
+            Scanner sc = new Scanner(system.in);
+            double x = sc.nextDouble();
+            System.out.println(x*15/100);
+            string name = sc.nextLine();
+            int age = sc.nextInt();
+            System.out.println(name + " :" +age);`
+
+
+        }
+    }
+}
